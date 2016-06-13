d448a3d17c4f4bf118298e433c5d8f6811c3b813
aStieber <Andrew.Stieber@colorado.edu>
2016-06-12 23:35:43 -0600

    fixed image sizes

--- CribbageCalc.py
+++ CribbageCalc.py
@@ -1,4 +1,4 @@
-import sys, os, random, analysis, math, bicycle, time
+import sys, os, random, analysis, math, bicycle
 from tkinter import *
 
 def addCard(event):
@@ -24,15 +24,17 @@ def addCard(event):
 			print("ah shit mate your entry was fucked")
 
 def calcUserEntry():
-	switch()
-	if (len(varHand) == 4):
+	if (len(varHand.bigHand) == 4):
 		hPhase = analysis.handPhase(varHand)
 		hPhase.analyze4Hand
-	elif (len(varHand) == 6):
+	elif (len(varHand.bigHand) == 6):
 		hPhase = analysis.handPhase(varHand)
 		hPhase.analyze6Hand
 	else:
 		print("Yo you need 4 or 6 cards")
+
+def calcRandomEntry():
+	pass
 
 def getImageFilename(card):
 	x = "%s/gif/%s%02d.gif" % (os.getcwd(), card.getSuitChar(), card.value)
@@ -41,29 +43,33 @@ def getImageFilename(card):
 
 
 def updateCards(): #first, populate with all 6, then dim those who aren't the best (if known)
-	cards = [None] * 6
+	
+	cardID = [None] * 6
 	count = 0
 	for x in varHand.bigHand:
-		xLoc = 120 + 110 * count #yLoc is 160
-		f = PhotoImage(file=getImageFilename(x))
+		xLoc = 85 + 110 * count #yLoc is 160
+		if cards[count]:
+			f = cards[count]
+		else:
+			f = PhotoImage(file=getImageFilename(x))
 
-		cards[count] = cardDisplay.create_image((xLoc, 90), image=f)
+		cardID[count] = cardDisplay.create_image(xLoc, 90, image=f)
+		cards[count] = f
 	
 		count += 1
 
 def update():
 	updateCards()
-	cardDisplay.update()
-
-
-	root.after(1000, update)
+	root.after(500, update)
 
 
 root = Tk()
-root.geometry("720x560")
+#root.geometry("720x560")
 varHand = bicycle.Hand()
-userInput = StringVar()
 hPhase = analysis.handPhase()
+userInput = StringVar()
+cards = [None] * 6 #keeps images on the canvas, needs to be here
+
 
 Label(root, text="Enter Cards\n(JH is Jack of Hearts)").grid(row=0, column=0)
 
@@ -72,10 +78,11 @@ cardEntryWidget.grid(row=1, column=0) #this returns None
 root.bind('<Return>', addCard)
 
 Button(root, text="Find Best", command=calcUserEntry).grid(row=2, column=0)
+Button(root, text="Random", command=calcRandomEntry).grid(row=3, column=0)
 
 #card display
-cardDisplay = Canvas(root, width=720, height=360, bg='cyan')
-cardDisplay.grid(row=3, rowspan=2, columnspan=2)
+cardDisplay = Canvas(root, width=720, height=180, bg='#084F3D')
+cardDisplay.grid(row=5, rowspan=2, columnspan=2)
 
 
 #run my loop
